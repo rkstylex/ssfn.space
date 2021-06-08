e5a960902ace76ce60a5ef5f8ffc6d53d6fa14c0
raicho <raioosered@gmail.com>
2021-06-08 09:08:07 +0300

    Added navbar

---
+++ assets/app.js
@@ -0,0 +1,14 @@
+//style //
+import './styles/app.css';
+
+//interactive //
+
+// jquery-global.js
+import jquery from 'jquery';
+window.jQuery = jquery;
+window.$ = jquery;
+import './bootstrap';
+import 'alpinejs';
+
+
+import './custom';
