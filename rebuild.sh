721b59b1cfe917c76e92b9e535d0395916e58233
raicho <raioosered@gmail.com>
2021-06-14 19:58:17 +0300

    Rebuild.sh updated and composer.phar added

--- rebuild.sh
+++ rebuild.sh
@@ -4,12 +4,21 @@ NC='\033[0m' # No Color
 echo "========================================"
 echo -e "${RED} ===== W A R N I N G ====="
 echo -e "${NC}========================================"
-read -p "This script will remove all assets in public/build and public/media folders. Are you sure to continue? y/n ? : " CONT
+read -p "This script will remove important folders. Are you sure to continue? y/n ? : " CONT
 
 if [ "$CONT" = "y" ]; then
-   echo -e "${RED} Removing public/build folder"
-   echo -e "${RED} Removing public/media folder"
+
+  echo -e "${RED} ===== C O M P O S E R ====="
+  echo -e "${RED} Removing vendor folder"
+  rm -rf vendor
+  php composer.phar install
+  echo -e "${RED} Removing public/build folder"
+  echo -e "${RED} Removing public/media folder"
   rm -rf public/build
+  echo -e "${RED} Removing node_modules folder"
+  rm -rf node_modules
+  echo -e "${RED} Npm installation started."
+  npm install
   echo -e "${RED} Rebuilding in progress please wait"
   npm run build
   echo -e "${RED} BUILD SUCCESSFULL"
